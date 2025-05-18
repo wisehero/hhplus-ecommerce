8624eddee37fa67f3478a3702e6533fdb80fd75f
wisehero <godjw96@gmail.com>
2025-05-18 19:52:31 +0900

    feat : @Async 가 동작할 수 있도록 @EnableAsync 추가

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -3,8 +3,10 @@ package kr.hhplus.be.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableRetry
+@EnableAsync
 @SpringBootApplication
 public class ServerApplication {
 
