f0092fb6b5d86865cd0df0e3d98e53a312f0a37a
wisehero <godjw96@gmail.com>
2025-05-22 23:33:29 +0900

    feat : 이벤트 핸들러 & 이벤트 퍼블리셔 테스트

---
+++ src/test/java/kr/hhplus/be/server/infra/point/spring/OrderPaymentEventPublisherTest.java
@@ -0,0 +1,48 @@
+package kr.hhplus.be.server.infra.point.spring;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
+
+import kr.hhplus.be.server.domain.order.Order;
+import kr.hhplus.be.server.domain.point.event.type.PaymentSuccessEvent;
+import kr.hhplus.be.server.domain.user.User;
+
+@SpringBootTest
+@RecordApplicationEvents
+class PointPaymentEventPublisherTest {
+
+	@MockitoSpyBean
+	private PointPaymentEventPublisher eventPublisher;
+
+	@Autowired
+	private ApplicationEvents applicationEvents;
+
+	@Test
+	@DisplayName("결제 완료 이벤트 발행시, ApplicationEventPublisher를 통해 이벤트가 발행된다")
+	void shouldPublishEventThroughApplicationEventPublisher() {
+		// given
+		Order order = Order.create(User.create(1L));
+		PaymentSuccessEvent event = new PaymentSuccessEvent(order);
+
+		// when
+		eventPublisher.publish(event);
+
+		// then
+		verify(eventPublisher).publish(any(PaymentSuccessEvent.class));
+		assertThat(applicationEvents.stream(PaymentSuccessEvent.class).count()).isEqualTo(1);
+
+		PaymentSuccessEvent publishedEvent = applicationEvents.stream(PaymentSuccessEvent.class)
+			.findFirst()
+			.orElseThrow();
+
+		assertThat(publishedEvent.getOrder()).isEqualTo(order);
+	}
+}
--- src/test/java/kr/hhplus/be/server/integration/PointOrderPaymentFacadeIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/integration/PointOrderPaymentFacadeIntegrationTest.java
@@ -1,19 +1,21 @@
 package kr.hhplus.be.server.integration;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.awaitility.Awaitility.*;
 import static org.instancio.Select.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
 
 import kr.hhplus.be.server.application.point.PointOrderPaymentFacade;
 import kr.hhplus.be.server.application.point.dto.PointOrderPaymentCommand;
@@ -24,12 +26,15 @@ import kr.hhplus.be.server.domain.order.client.DataPlatformClient;
 import kr.hhplus.be.server.domain.point.Balance;
 import kr.hhplus.be.server.domain.point.Point;
 import kr.hhplus.be.server.domain.point.PointRepository;
+import kr.hhplus.be.server.domain.point.event.type.PaymentSuccessEvent;
 import kr.hhplus.be.server.domain.product.Product;
 import kr.hhplus.be.server.domain.product.ProductRepository;
 import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.domain.user.UserRepository;
+import kr.hhplus.be.server.interfaces.event.order.OrderPaymentEventHandler;
 import kr.hhplus.be.server.support.IntgerationTestSupport;
 
+@RecordApplicationEvents
 public class PointOrderPaymentFacadeIntegrationTest extends IntgerationTestSupport {
 
 	@Autowired
@@ -47,8 +52,14 @@ public class PointOrderPaymentFacadeIntegrationTest extends IntgerationTestSuppo
 	@Autowired
 	private ProductRepository productRepository;
 
-	@MockitoBean
+	@Autowired
+	private ApplicationEvents applicationEvents;
+
+	@MockitoSpyBean
 	private DataPlatformClient dataPlatformClient;
+
+	@MockitoSpyBean
+	private OrderPaymentEventHandler orderPaymentEventHandler;
 
 	@Test
 	@DisplayName("유저 포인트로 결제 시 주문 상태가 PAID로 변경되고 포인트가 차감되며 외부 시스템 전송이 발생한다.")
@@ -68,8 +79,6 @@ public class PointOrderPaymentFacadeIntegrationTest extends IntgerationTestSuppo
 
 		Order order = Order.create(user);
 		order.addOrderProduct(product, 2L); // 총액: 60,000
-		ReflectionTestUtils.setField(order, "createdAt", LocalDateTime.now());
-		ReflectionTestUtils.setField(order, "updatedAt", LocalDateTime.now());
 
 		Order savedOrder = orderRepository.save(order);
 
@@ -84,9 +93,18 @@ public class PointOrderPaymentFacadeIntegrationTest extends IntgerationTestSuppo
 
 		assertAll(
 			() -> assertThat(updatedOrder.getOrderStatus()).isEqualTo(OrderStatus.PAID),
-			() -> assertThat(userPoint.getAmount()).isEqualByComparingTo(BigDecimal.valueOf(40_000)),
-			// 100_000 - 60_000
-			() -> verify(dataPlatformClient).send(any(Order.class))
-		);
+			() -> assertThat(userPoint.getAmount()).isEqualByComparingTo(BigDecimal.valueOf(40_000)));
+
+		// 이벤트 발행 검증
+		assertThat(applicationEvents.stream(PaymentSuccessEvent.class).count()).isEqualTo(1);
+
+		await().atMost(5, TimeUnit.SECONDS)
+			.untilAsserted(() -> {
+				verify(orderPaymentEventHandler).handleOrderPaymentEvent(any(PaymentSuccessEvent.class));
+			});
+
+		await().atMost(5, TimeUnit.SECONDS)
+			.untilAsserted(() -> verify(dataPlatformClient).send(any(Order.class)));
+
 	}
 }
